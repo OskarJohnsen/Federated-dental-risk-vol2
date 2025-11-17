b515ba8f3fecfb236bab567f024af2702eed6199
jonas <smoothyy.3@gmail.com>
2025-11-17 16:07:21 +0100

    rework weird onehot encoding process

--- src/digit_fr/ml/data/datasets.py
+++ src/digit_fr/ml/data/datasets.py
@@ -18,9 +18,21 @@ def create_data_loaders(data, batch_size=32, shuffle_train=True):
     train_dataset = MultiTaskDataset(data['train']['X'], data['train']['y_classification'])
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle_train)
     
+    loaders = [train_loader]
+    
+    if 'val' in data:
+        val_dataset = MultiTaskDataset(data['val']['X'], data['val']['y_classification'])
+        val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+        loaders.append(val_loader)
+    
     if 'test' in data:
         test_dataset = MultiTaskDataset(data['test']['X'], data['test']['y_classification'])
         test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
-        return train_loader, test_loader
+        loaders.append(test_loader)
     
-    return train_loader
+    if len(loaders) == 3:
+        return train_loader, loaders[1], loaders[2]
+    elif len(loaders) == 2:
+        return train_loader, loaders[1]
+    else:
+        return train_loader
--- src/digit_fr/ml/data/loaders.py
+++ src/digit_fr/ml/data/loaders.py
@@ -1,7 +1,9 @@
 from ...core.paths import root_path
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
 
 def load_raw_data():
     df = pd.read_csv(root_path('data', 'raw', 'fed_recommenders_synthetic_dataset.csv'))
@@ -19,28 +21,89 @@ def load_raw_data():
             missing_indicator_name = f'{col}_MISSING'
             X[missing_indicator_name] = X[col].isnull().astype(float)
     
-    # onehot only nominal features
-    features_to_onehot = ['Surgical_Extraction_Type', 'Tooth_Angulation']
-    
-    for col in features_to_onehot:
-        if col in X.columns:
-            dummies = pd.get_dummies(X[col], prefix=col, drop_first=False, dtype=float)
-            X = X.drop(columns=[col])
-            X = pd.concat([X, dummies], axis=1)
-    
-    # impute missing values # TODO
-    imputer = SimpleImputer(strategy='median')
-    X = pd.DataFrame(imputer.fit_transform(X), columns=X.columns, index=X.index)
-    
     return {'X': X, 'y_classification': y_classification}
 
 
-def load_data_with_split(test_size=0.2, random_state=42):
+def load_data_with_split(test_size=0.2, val_size=0.2, random_state=42):
+
     data = load_raw_data()
     
-    X_train, X_test, y_classification_train, y_classification_test = train_test_split(data['X'], data['y_classification'], test_size=test_size, random_state=random_state)
+    X_temp, X_test, y_temp, y_test = train_test_split(
+        data['X'], 
+        data['y_classification'], 
+        test_size=test_size, 
+        random_state=random_state
+    )
+    
+    val_size_adjusted = val_size / (1 - test_size)
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_temp,
+        y_temp,
+        test_size=val_size_adjusted,
+        random_state=random_state
+    )
+    
+    features_to_onehot = ['Surgical_Extraction_Type', 'Tooth_Angulation']
+    
+    categorical_cols = [col for col in features_to_onehot if col in X_train.columns]
+    numerical_cols = [col for col in X_train.columns if col not in categorical_cols]
+    
+    X_train_encoded = X_train.copy()
+    X_val_encoded = X_val.copy()
+    X_test_encoded = X_test.copy()
+    
+    if categorical_cols:
+        ohe = OneHotEncoder(drop=None, sparse_output=False, handle_unknown='ignore', dtype=np.float32)
+        ohe.fit(X_train[categorical_cols])
+        
+        ohe_feature_names = ohe.get_feature_names_out(categorical_cols)
+        
+        X_train_ohe = pd.DataFrame(
+            ohe.transform(X_train[categorical_cols]),
+            columns=ohe_feature_names,
+            index=X_train.index
+        )
+        X_val_ohe = pd.DataFrame(
+            ohe.transform(X_val[categorical_cols]),
+            columns=ohe_feature_names,
+            index=X_val.index
+        )
+        X_test_ohe = pd.DataFrame(
+            ohe.transform(X_test[categorical_cols]),
+            columns=ohe_feature_names,
+            index=X_test.index
+        )
+        
+        X_train_encoded = X_train_encoded.drop(columns=categorical_cols)
+        X_val_encoded = X_val_encoded.drop(columns=categorical_cols)
+        X_test_encoded = X_test_encoded.drop(columns=categorical_cols)
+        
+        X_train_encoded = pd.concat([X_train_encoded, X_train_ohe], axis=1)
+        X_val_encoded = pd.concat([X_val_encoded, X_val_ohe], axis=1)
+        X_test_encoded = pd.concat([X_test_encoded, X_test_ohe], axis=1)
+    
+    X_val_encoded = X_val_encoded.reindex(columns=X_train_encoded.columns, fill_value=0)
+    X_test_encoded = X_test_encoded.reindex(columns=X_train_encoded.columns, fill_value=0)
+    
+    imputer = SimpleImputer(strategy='median')
+    X_train_imputed = pd.DataFrame(
+        imputer.fit_transform(X_train_encoded), 
+        columns=X_train_encoded.columns, 
+        index=X_train_encoded.index
+    )
+    X_val_imputed = pd.DataFrame(
+        imputer.transform(X_val_encoded), 
+        columns=X_val_encoded.columns, 
+        index=X_val_encoded.index
+    )
+    X_test_imputed = pd.DataFrame(
+        imputer.transform(X_test_encoded), 
+        columns=X_test_encoded.columns, 
+        index=X_test_encoded.index
+    )
     
     return {
-        'train': {'X': X_train, 'y_classification': y_classification_train},
-        'test': {'X': X_test, 'y_classification': y_classification_test}
+        'train': {'X': X_train_imputed, 'y_classification': y_train},
+        'val': {'X': X_val_imputed, 'y_classification': y_val},
+        'test': {'X': X_test_imputed, 'y_classification': y_test}
     }
