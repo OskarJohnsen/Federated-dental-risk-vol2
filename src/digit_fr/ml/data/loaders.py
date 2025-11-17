e91953c6a5afa6b2bd027286f7791357c78eaefe
jonas <smoothyy.3@gmail.com>
2025-11-17 16:18:08 +0100

    adjust

--- src/digit_fr/ml/data/loaders.py
+++ src/digit_fr/ml/data/loaders.py
@@ -25,85 +25,40 @@ def load_raw_data():
 
 
 def load_data_with_split(test_size=0.2, val_size=0.2, random_state=42):
-
     data = load_raw_data()
-    
-    X_temp, X_test, y_temp, y_test = train_test_split(
-        data['X'], 
-        data['y_classification'], 
-        test_size=test_size, 
-        random_state=random_state
-    )
-    
+
+    X_temp, X_test, y_temp, y_test = train_test_split(data['X'],data['y_classification'],test_size=test_size,random_state=random_state,)
+
     val_size_adjusted = val_size / (1 - test_size)
-    X_train, X_val, y_train, y_val = train_test_split(
-        X_temp,
-        y_temp,
-        test_size=val_size_adjusted,
-        random_state=random_state
-    )
-    
+    X_train, X_val, y_train, y_val = train_test_split(X_temp,y_temp,test_size=val_size_adjusted,random_state=random_state,)
+
     features_to_onehot = ['Surgical_Extraction_Type', 'Tooth_Angulation']
-    
-    categorical_cols = [col for col in features_to_onehot if col in X_train.columns]
-    numerical_cols = [col for col in X_train.columns if col not in categorical_cols]
-    
-    X_train_encoded = X_train.copy()
-    X_val_encoded = X_val.copy()
-    X_test_encoded = X_test.copy()
-    
+    categorical_cols = [c for c in features_to_onehot if c in X_train.columns]
+
     if categorical_cols:
-        ohe = OneHotEncoder(drop=None, sparse_output=False, handle_unknown='ignore', dtype=np.float32)
+        ohe = OneHotEncoder(drop=None,sparse_output=False,handle_unknown='ignore',dtype=np.float32,)
         ohe.fit(X_train[categorical_cols])
-        
         ohe_feature_names = ohe.get_feature_names_out(categorical_cols)
-        
-        X_train_ohe = pd.DataFrame(
-            ohe.transform(X_train[categorical_cols]),
-            columns=ohe_feature_names,
-            index=X_train.index
-        )
-        X_val_ohe = pd.DataFrame(
-            ohe.transform(X_val[categorical_cols]),
-            columns=ohe_feature_names,
-            index=X_val.index
-        )
-        X_test_ohe = pd.DataFrame(
-            ohe.transform(X_test[categorical_cols]),
-            columns=ohe_feature_names,
-            index=X_test.index
-        )
-        
-        X_train_encoded = X_train_encoded.drop(columns=categorical_cols)
-        X_val_encoded = X_val_encoded.drop(columns=categorical_cols)
-        X_test_encoded = X_test_encoded.drop(columns=categorical_cols)
-        
-        X_train_encoded = pd.concat([X_train_encoded, X_train_ohe], axis=1)
-        X_val_encoded = pd.concat([X_val_encoded, X_val_ohe], axis=1)
-        X_test_encoded = pd.concat([X_test_encoded, X_test_ohe], axis=1)
-    
-    X_val_encoded = X_val_encoded.reindex(columns=X_train_encoded.columns, fill_value=0)
-    X_test_encoded = X_test_encoded.reindex(columns=X_train_encoded.columns, fill_value=0)
-    
+
+        def add_ohe(X):
+            X_ohe = pd.DataFrame(ohe.transform(X[categorical_cols]),columns=ohe_feature_names,index=X.index,)
+            X_new = X.drop(columns=categorical_cols)
+            return pd.concat([X_new, X_ohe], axis=1)
+
+        X_train = add_ohe(X_train)
+        X_val = add_ohe(X_val)
+        X_test = add_ohe(X_test)
+
+    X_val  = X_val.reindex(columns=X_train.columns, fill_value=0)
+    X_test = X_test.reindex(columns=X_train.columns, fill_value=0)
+
     imputer = SimpleImputer(strategy='median')
-    X_train_imputed = pd.DataFrame(
-        imputer.fit_transform(X_train_encoded), 
-        columns=X_train_encoded.columns, 
-        index=X_train_encoded.index
-    )
-    X_val_imputed = pd.DataFrame(
-        imputer.transform(X_val_encoded), 
-        columns=X_val_encoded.columns, 
-        index=X_val_encoded.index
-    )
-    X_test_imputed = pd.DataFrame(
-        imputer.transform(X_test_encoded), 
-        columns=X_test_encoded.columns, 
-        index=X_test_encoded.index
-    )
-    
+    X_train = pd.DataFrame(imputer.fit_transform(X_train),columns=X_train.columns,index=X_train.index,)
+    X_val = pd.DataFrame(imputer.transform(X_val),columns=X_val.columns,index=X_val.index)
+    X_test = pd.DataFrame(imputer.transform(X_test),columns=X_test.columns,index=X_test.index,)
+
     return {
-        'train': {'X': X_train_imputed, 'y_classification': y_train},
-        'val': {'X': X_val_imputed, 'y_classification': y_val},
-        'test': {'X': X_test_imputed, 'y_classification': y_test}
+        'train': {'X': X_train, 'y_classification': y_train},
+        'val': {'X': X_val, 'y_classification': y_val},
+        'test': {'X': X_test, 'y_classification': y_test},
     }
